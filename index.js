86635ec869128ff85add2d9ba45ae4de8e274f67
webdesigner295 <webdesigner295@gmail.com>
2023-12-06 23:50:45 +0500

    fixing cors error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+// import cors from "cors";
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -15,6 +16,13 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
